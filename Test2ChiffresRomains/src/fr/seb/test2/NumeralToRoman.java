78aa372bf1bca4adaff39519fec866256c18023b
c18004286 <c18004286@a-f004246.salsa.univ-amu.fr>
2019-02-28 17:18:36 +0100

    Correction de NumeralToRoman dans sa méthode getRoman()

--- Test2ChiffresRomains/src/fr/seb/test2/NumeralToRoman.java
+++ Test2ChiffresRomains/src/fr/seb/test2/NumeralToRoman.java
@@ -4,40 +4,41 @@ public class NumeralToRoman {
 	
 	public String getRoman(Integer nbentier) {
 		String roman="";
+		while(nbentier!=0){
+			if(nbentier>=1000) {
+				roman+="M";
+				nbentier-=1000;
+			}
 		
-		if(nbentier>=1000) {
-			roman+="M";
-			nbentier-=1000;
-		}
+			if(nbentier>=500) {
+				roman+="D";
+				nbentier-=500;
+			}
 		
-		if(nbentier>=500) {
-			roman+="D";
-			nbentier-=500;
-		}
+			if(nbentier>=100) {
+				roman+="C";
+				nbentier-=100;
+			}
 		
-		if(nbentier>=100) {
-			roman+="C";
-			nbentier-=100;
-		}
+			if(nbentier>=50) {
+				roman+="L";
+				nbentier-=50;
+			}
 		
-		if(nbentier>=50) {
-			roman+="L";
-			nbentier-=50;
-		}
+			if(nbentier>=10) {
+				roman+="X";
+				nbentier-=10;
+			}
 		
-		if(nbentier>=10) {
-			roman+="X";
-			nbentier-=10;
-		}
+			if(nbentier>=5) {
+				roman+="V";
+				nbentier-=5;
+			}
 		
-		if(nbentier>=5) {
-			roman+="V";
-			nbentier-=5;
-		}
-		
-		if(nbentier>=1) {
-			roman+="I";
-			nbentier-=1;
+			if(nbentier>=1) {
+				roman+="I";
+				nbentier-=1;
+			}
 		}
 		return roman;
 	}
